4fe5703dd4e3c0c6a8099f4859f91ccb05f22079
Noah Bass <bassjnoah@gmail.com>
2016-09-25 00:30:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# revolutionuc-live
+
+## api
+
+Example post request to publish push notification: `curl -d "apikey=ffreffwwrew&payload=My Message" http://localhost:3000/api/v1/notification/publish` (add a `-v` flag for a verbose message)
+
+## Notes
+
+- RevolutionUC fall 2016 colors: #DB0010 and #8E000A
+- Filling out the mentor form sends emails to mentors with form information
+
+## TODO
+
+- Add support for custom notification icon
+- Add newbie hacker guides (ex: [pennapps hacker guide](http://mentoring.pennapps.com/guide/))
---
+++ app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+require('dotenv').config();
+
+const express  = require('express');
+const expressValidator = require('express-validator');
+const path     = require('path');
+const favicon  = require('serve-favicon');
+const logger   = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'lib', 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Controllers
+const pages = require('./lib/controllers/pages');
+const notificationApi = require('./lib/controllers/api/v1/notification');
+
+// Routes
+app.get('/', pages.index);
+app.get('/schedule', pages.schedule);
+app.get('/sponsors', pages.sponsors);
+app.get('/notifications', pages.notifications);
+
+// Notification api
+app.post('/api/v1/notification/publish', notificationApi.publish);
+app.post('/api/v1/notification/subscribe', notificationApi.subscribe);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
